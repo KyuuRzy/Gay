959a1ae815502e92368ed563b6bc644a7a710716
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-06 01:09:15 +0800

    Update motivation.js

--- motivation.js
+++ motivation.js
@@ -1,31 +1,31 @@
 var motivations = [
   `menjadi diri sendiri lebih baik dari pada menjadi orang lain karena kita tidak tahu resiko apa yang di ambil orang lain disaat kita ingin menjadi dirinya"
   
-~ RexxzynXD`,
+"~ RexxzynXD`,
   `orang lain hanya bisa menilai sifat,ekonomi, karakteristik kita, tapi mereka tidak bisa tidak bisa menilai sifat, ekonomi, karakteristik mereka Sendiri"
   
-~ RexxzynXD`,
+"~ RexxzynXD`,
   `berbohong terhadap kejujuran bukan lah hal yang buruk"
   
-~ RexxzynXD`,
+"~ RexxzynXD`,
   `Balas dendam dengan cara yang bodoh adalah titik di mana kamu membalasnya dengan titik buta tidak menemukan cara lainya
   
-~ RexxzynXD`,
+"~ RexxzynXD`,
   `diungkit atau mengungkit lebih untung mana?, diungkit? atau mengungkit? Dua duanya tidak untung terhadap dirimu sendiri memilih diam adalah cara yang bagus agar dua hal itu tidak terjadi"
 
-~ RexxzynXD`,
+"~ RexxzynXD`,
   `Nothing lasts forever, we can change the future"
 
-~ RexxzynXD`,
+"~ RexxzynXD`,
   `percaya atau tidak percaya kita hanya bisa membuktikannya dengan hasil bukan sekedar kata"
 
-~ RexxzynXD`,
+"~ RexxzynXD`,
   `Dalam dunia terkutuk ini, kedamaian dimana setiap orang saling mengerti hanyalah sebuah angan-angan."
 
-~ Surya`,
+"~ Surya`,
   `apapun masalahmu tetap berada di jalan Allah sobat."
 
-~ Hyuu Official`
+"~ Hyuu Official`
 ];
 
 var randomMotivation =
