e867d0cd956b7ff01f04e9f7377cc957a69502ee
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-05 21:35:54 +0800

    Update motivation.js

--- motivation.js
+++ motivation.js
@@ -8,6 +8,18 @@ var motivations = [
   `berbohong terhadap kejujuran bukan lah hal yang buruk
   
   -RexxzynXD`,
+  `Balas dendam dengan cara yang bodoh adalah titik di mana kamu membalasnya dengan titik buta tidak menemukan cara lainya
+
+  -RexxzynXD`,
+  `diungkit atau mengungkit lebih untung mana?, diungkit? atau mengungkit? Dua duanya tidak untung terhadap dirimu sendiri memilih diam adalah cara yang bagus agar dua hal itu tidak terjadi
+
+  -RexxzynXD`,
+  `Nothing lasts forever, we can change the future
+
+  -RexxzynXD`,
+  `percaya atau tidak percaya kita hanya bisa membuktikannya dengan hasil bukan sekedar kata
+
+  -RexxzynXD`,
 ];
 
 var randomMotivation =
