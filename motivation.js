a3d1b7b2c42e129415e9a09110ee56af04ef7656
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-06 02:12:26 +0800

    Update motivation.js

--- motivation.js
+++ motivation.js
@@ -1,37 +1,15 @@
 var motivations = [
-  `menjadi diri sendiri lebih baik dari pada menjadi orang lain karena kita tidak tahu resiko apa yang di ambil orang lain disaat kita ingin menjadi dirinya"
-  
-"RexxzynXD`,
-  `orang lain hanya bisa menilai sifat,ekonomi, karakteristik kita, tapi mereka tidak bisa tidak bisa menilai sifat, ekonomi, karakteristik mereka Sendiri"
-  
-"RexxzynXD`,
-  `berbohong terhadap kejujuran bukan lah hal yang buruk"
-  
-"RexxzynXD`,
-  `Balas dendam dengan cara yang bodoh adalah titik di mana kamu membalasnya dengan titik buta tidak menemukan cara lainya
-  
-"RexxzynXD`,
-  `diungkit atau mengungkit lebih untung mana?, diungkit? atau mengungkit? Dua duanya tidak untung terhadap dirimu sendiri memilih diam adalah cara yang bagus agar dua hal itu tidak terjadi"
-
-"RexxzynXD`,
-  `Nothing lasts forever, we can change the future"
-
-"RexxzynXD`,
-  `percaya atau tidak percaya kita hanya bisa membuktikannya dengan hasil bukan sekedar kata"
-
-"RexxzynXD`,
-  `Dalam dunia terkutuk ini, kedamaian dimana setiap orang saling mengerti hanyalah sebuah angan-angan."
-
-"Surya`,
-  `apapun masalahmu tetap berada di jalan Allah sobat."
-
-"Hyuu Official`
-  `walau sendirian saya tidak akan Tumbang"
-
-"KyuuRzy`,
-`Gak Takut Di Benci dan gak takut di musuhi, saya solo pun masih bisa berdiri."
-
-"KyuuRzy`,
+  `menjadi diri sendiri lebih baik dari pada menjadi orang lain karena kita tidak tahu resiko apa yang di ambil orang lain disaat kita ingin menjadi dirinya\n\nRexxzynXD`,
+  `orang lain hanya bisa menilai sifat,ekonomi, karakteristik kita, tapi mereka tidak bisa tidak bisa menilai sifat, ekonomi, karakteristik mereka Sendiri\n\nRexxzynXD`,
+  `berbohong terhadap kejujuran bukan lah hal yang buruk\n\nRexxzynXD`,
+  `Balas dendam dengan cara yang bodoh adalah titik di mana kamu membalasnya dengan titik buta tidak menemukan cara lainya\n\nRexxzynXD`,
+  `diungkit atau mengungkit lebih untung mana?, diungkit? atau mengungkit? Dua duanya tidak untung terhadap dirimu sendiri memilih diam adalah cara yang bagus agar dua hal itu tidak terjadi\n\nRexxzynXD`,
+  `Nothing lasts forever, we can change the future"\n\nRexxzynXD`,
+  `percaya atau tidak percaya kita hanya bisa membuktikannya dengan hasil bukan sekedar kata\n\nRexxzynXD`,
+  `Dalam dunia terkutuk ini, kedamaian dimana setiap orang saling mengerti hanyalah sebuah angan-angan.\n\nSurya`,
+  `apapun masalahmu tetap berada di jalan Allah sobat.\n\nHyuu Official`
+  `walau sendirian saya tidak akan Tumbang\n\nKyuuRzy`,
+  `Gak Takut Di Benci dan gak takut di musuhi, saya solo pun masih bisa berdiri.\n\nKyuuRzy`,
  ];
 
 
