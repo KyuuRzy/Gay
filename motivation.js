9ce3eca41d3544db2806e6389f7740ca22736fa4
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-05 21:05:47 +0800

    Update motivation.js

--- motivation.js
+++ motivation.js
@@ -1,7 +1,7 @@
 var motivations = [
-  "menjadi diri sendiri lebih baik dari pada menjadi orang lain karena kita tidak tahu resiko apa yang di ambil orang lain disaat kita ingin menjadi dirinya\n\n-RexxzynXD",
-  "orang lain hanya bisa menilai sifat,ekonomi, karakteristik kita, tapi mereka tidak bisa tidak bisa menilai sifat, ekonomi, karakteristik mereka Sendiri\n\n-RexxzynXD",
-  "berbohong terhadap kejujuran bukan lah hal yang buruk\n\n-RexxzynXD",
+  `menjadi diri sendiri lebih baik dari pada menjadi orang lain karena kita tidak tahu resiko apa yang di ambil orang lain disaat kita ingin menjadi dirinya\n\n-RexxzynXD`,
+  `orang lain hanya bisa menilai sifat,ekonomi, karakteristik kita, tapi mereka tidak bisa tidak bisa menilai sifat, ekonomi, karakteristik mereka Sendiri\n\n-RexxzynXD`,
+  `berbohong terhadap kejujuran bukan lah hal yang buruk\n\n-RexxzynXD`,
 ];
 
 var randomMotivation =
