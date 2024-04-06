ea51bf0d75c02f7f769a28b2fcb166bb4b1c4306
KyuuRzy <160400183+KyuuRzy@users.noreply.github.com>
2024-04-06 21:02:35 +0800

    Update motivation.js

--- motivation.js
+++ motivation.js
@@ -2,6 +2,7 @@ var motivations = [
   "menjadi diri sendiri lebih baik dari pada menjadi orang lain karena kita tidak tahu resiko apa yang di ambil orang lain disaat kita ingin menjadi dirinya\n\n𝗥𝗲𝘅𝘅𝘇𝘆𝗻𝗫𝗗",
   "orang lain hanya bisa menilai sifat,ekonomi, karakteristik kita, tapi mereka tidak bisa tidak bisa menilai sifat, ekonomi, karakteristik mereka Sendiri\n\n𝗥𝗲𝘅𝘅𝘇𝘆𝗻𝗫𝗗",
   "berbohong terhadap kejujuran bukan lah hal yang buruk\n\n𝗥𝗲𝘅𝘅𝘇𝘆𝗻𝗫𝗗",
+  "Tidak ada kata kata. Yang penting bukti nyata\n\n𝗡𝗮𝗻𝘀 𝗢𝗳𝗳𝗶𝗰𝗶𝗮𝗹",
   "Gas Terus Abangkuh🔥! Jalanan kita punya, mimpi kita raih!💯\n\n𝗧𝗵𝗲𝗭𝗲𝘁𝘀𝘂𝗯𝗼𝗫𝘆𝗴𝗲𝗻",
   "Jika Belum Mendapatkan Yang Baik, Berarti Dirimu Sendiri Belum Baik\n\n𝗔𝗻-𝗻𝘂𝗿:𝟮𝟲",
   "Balas dendam dengan cara yang bodoh adalah titik di mana kamu membalasnya dengan titik buta tidak menemukan cara lainya\n\n𝗥𝗲𝘅𝘅𝘇𝘆𝗻𝗫𝗗",
